001b6c5f4acd14a296d13ce24df0071932177565
Tony Sullivan <tony.f.sullivan@outlook.com>
2021-08-25 16:20:51 +0000

    Populating the full UI from Forestry (#1)
    * WIP: hooking up to the forestry data
    
    * Hooking up most of the NewsletterBlock
    
    * finishing up the CMS integration
    
    Co-authored-by: Tony Sullivan <tony.f.sullivan@gmail.com>

---
+++ types/cms.d.ts
@@ -0,0 +1,110 @@
+declare namespace CMS {
+  type Markdown = string;
+
+  interface Navigation {
+    main: [
+      {
+        title: string;
+        href: string;
+      }
+    ];
+    legal: [
+      {
+        title: string;
+        href: string;
+      }
+    ];
+  }
+
+  interface Site {
+    title: string;
+    description: string;
+    url: string;
+    domain: string;
+    author: string;
+    social: {
+      twitter_card: string;
+    };
+  }
+
+  interface Image {
+    src: string;
+    alt: string;
+  }
+
+  interface CallToAction {
+    text: string;
+    url: string;
+  }
+
+  interface Form {
+    fields: {
+      id: string;
+      title: string;
+      placeholder: string;
+      required: boolean;
+    }[];
+    submitText: string;
+    footnote?: string;
+  }
+
+  interface Card {
+    title: string;
+    subtitle: string;
+    content: Markdown;
+    image: Image;
+  }
+
+  interface CardBlock {
+    template: "cardblock";
+    title: string;
+    cards: Card[];
+  }
+
+  interface HeroBlock {
+    template: "heroblock";
+    title: string;
+    content: Markdown;
+    cta: CallToAction;
+  }
+
+  interface ImageBlock {
+    template: "imageblock";
+    title: string;
+    subtitle: string;
+    subtitleIcon: boolean;
+    flipped: boolean;
+    muted: boolean;
+    image: Image;
+    content: Markdown;
+    cta: CallToAction;
+  }
+
+  interface NewsletterBlock {
+    template: "newsletterblock";
+    title: string;
+    subtitle: string;
+    subtitleIcon: boolean;
+    flipped: boolean;
+    image: Image;
+    content: Markdown;
+    form: Form;
+  }
+
+  interface TextBlock {
+    template: "textblock";
+    title: string;
+    subtitle: string;
+    content: Markdown;
+    cta: CallToAction;
+  }
+
+  type Block = CardBlock | HeroBlock | ImageBlock | NewsletterBlock | TextBlock;
+
+  interface Page {
+    title: string;
+    description: string;
+    image: string;
+    blocks: Block[];
+  }
+}
